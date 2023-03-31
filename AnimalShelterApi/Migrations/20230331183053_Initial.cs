094b1fc6701a2e605df35a91fcb953bafb878247
Mike Donovan <mike@mikedonovan.dev>
2023-03-31 11:33:24 -0700

    Add seed values (pagination working)

--- AnimalShelterApi/Migrations/20230331183053_Initial.cs
+++ AnimalShelterApi/Migrations/20230331183053_Initial.cs
@@ -36,12 +36,30 @@ namespace AnimalShelterApi.Migrations
             migrationBuilder.InsertData(
                 table: "Animals",
                 columns: new[] { "AnimalId", "Breed", "Gender", "Name", "Species" },
-                values: new object[] { 1, "Chihuahua", "Male", "Conan", "dog" });
-
-            migrationBuilder.InsertData(
-                table: "Animals",
-                columns: new[] { "AnimalId", "Breed", "Gender", "Name", "Species" },
-                values: new object[] { 2, "Short Hair", "Female", "Curtesia", "cat" });
+                values: new object[,]
+                {
+                    { 1, "Chihuahua", "Male", "Conan", "dog" },
+                    { 2, "ShortHair", "Female", "Curtesia", "cat" },
+                    { 3, "GoldenRetriever", "Male", "Chauncey", "dog" },
+                    { 4, "ShortHair", "Male", "Curtis", "cat" },
+                    { 5, "Corgi", "Female", "Ginger", "dog" },
+                    { 6, "Corgi", "Male", "Winston", "dog" },
+                    { 7, "Siamese", "Male", "Sugar", "cat" },
+                    { 8, "Airedale", "Female", "Valerie", "dog" },
+                    { 9, "LongHair", "Male", "Mingus", "cat" },
+                    { 10, "Dachshund", "Male", "Dexter", "dog" },
+                    { 11, "ShortHair", "Male", "Mingus", "cat" },
+                    { 12, "Boxer", "Female", "Jasmine", "dog" },
+                    { 13, "Siamese", "Male", "Linus", "cat" },
+                    { 14, "Beagle", "Female", "Petunia", "dog" },
+                    { 15, "Bulldog", "Male", "Tiny", "dog" },
+                    { 16, "ShortHair", "Female", "Minnie", "cat" },
+                    { 17, "ShortHair", "Male", "Marvin", "cat" },
+                    { 18, "Pomeranian", "Male", "Fizzgig", "dog" },
+                    { 19, "LongHair", "Male", "Wolfie", "cat" },
+                    { 20, "Husky", "Female", "Pandora", "dog" },
+                    { 21, "ShihTzu", "Male", "Batman", "dog" }
+                });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
--- AnimalShelterApi/Models/AnimalShelterApiContext.cs
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -14,7 +14,26 @@ namespace AnimalShelterApi.Models
     {
       builder.Entity<Animal>().HasData(
         new Animal { AnimalId = 1, Species = "dog", Breed = "Chihuahua", Gender = "Male", Name = "Conan" },
-        new Animal { AnimalId = 2, Species = "cat", Breed = "Short Hair", Gender = "Female", Name = "Curtesia"}
+        new Animal { AnimalId = 2, Species = "cat", Breed = "ShortHair", Gender = "Female", Name = "Curtesia"},
+        new Animal { AnimalId = 3, Species = "dog", Breed = "GoldenRetriever", Gender = "Male", Name = "Chauncey"},
+        new Animal { AnimalId = 4, Species = "cat", Breed = "ShortHair", Gender = "Male", Name = "Curtis"},
+        new Animal { AnimalId = 5, Species = "dog", Breed = "Corgi", Gender = "Female", Name = "Ginger"},
+        new Animal { AnimalId = 6, Species = "dog", Breed = "Corgi", Gender = "Male", Name = "Winston"},
+        new Animal { AnimalId = 7, Species = "cat", Breed = "Siamese", Gender = "Male", Name = "Sugar"},
+        new Animal { AnimalId = 8, Species = "dog", Breed = "Airedale", Gender = "Female", Name = "Valerie"},
+        new Animal { AnimalId = 9, Species = "cat", Breed = "LongHair", Gender = "Male", Name = "Mingus"},
+        new Animal { AnimalId = 10, Species = "dog", Breed = "Dachshund", Gender = "Male", Name = "Dexter"},
+        new Animal { AnimalId = 11, Species = "cat", Breed = "ShortHair", Gender = "Male", Name = "Mingus"},
+        new Animal { AnimalId = 12, Species = "dog", Breed = "Boxer", Gender = "Female", Name = "Jasmine"},
+        new Animal { AnimalId = 13, Species = "cat", Breed = "Siamese", Gender = "Male", Name = "Linus"},
+        new Animal { AnimalId = 14, Species = "dog", Breed = "Beagle", Gender = "Female", Name = "Petunia"},
+        new Animal { AnimalId = 15, Species = "dog", Breed = "Bulldog", Gender = "Male", Name = "Tiny"},
+        new Animal { AnimalId = 16, Species = "cat", Breed = "ShortHair", Gender = "Female", Name = "Minnie"},
+        new Animal { AnimalId = 17, Species = "cat", Breed = "ShortHair", Gender = "Male", Name = "Marvin"},
+        new Animal { AnimalId = 18, Species = "dog", Breed = "Pomeranian", Gender = "Male", Name = "Fizzgig"},
+        new Animal { AnimalId = 19, Species = "cat", Breed = "LongHair", Gender = "Male", Name = "Wolfie"},
+        new Animal { AnimalId = 20, Species = "dog", Breed = "Husky", Gender = "Female", Name = "Pandora"},
+        new Animal { AnimalId = 21, Species = "dog", Breed = "ShihTzu", Gender = "Male", Name = "Batman"}
       );
     }
   }
